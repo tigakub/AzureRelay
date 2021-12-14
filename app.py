e3256cf9935639fb70b43eaaada6f467f236bf3b
tigakub <tigakub@mac.com>
2021-12-13 20:38:56 -0800

    Create app.py
    Initial commit: Python script to relay JSON payload to ThingsBoard. Currently a simple echoing relay, and the ThingsBoard device ID is hardcoded.

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask import request
+import requests
+app = Flask(__name__)
+@app.route('/postjson', methods=['POST'])
+
+def relay():
+  if request.is_json:
+    dataDict = request.get_json()
+    headers = {'Content-Type': 'application/json'} 
+    req = requests.post('https://thingsboard.cloud/api/v1/Vxf3gbLjb0jhFtyz4kXJ/telemetry', json=dataDict, headers=headers)
+    return req.text
+  return 'Failed to extract payload'
